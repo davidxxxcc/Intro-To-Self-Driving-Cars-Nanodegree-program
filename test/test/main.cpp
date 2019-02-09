e6a95f6052d2d6d8a116784b491dc2201d6fb502
張辰豪 <daivdxxxcc@gmail.com>
2019-02-09 14:47:39 -0800

    test file

---
+++ test/test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 張辰豪 on 2/9/19.
+//  Copyright © 2019 Chen Hao Chang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
